53b7000caf4b0c1586b1725f585014838558d612
取舍 <494843897@qq.com>
2021-04-14 16:55:16 +0800

    修复 图标问题

---
+++ pages/tabbar/expand/expand.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<textView :content="content"/>
+		<itemList :itemList="items"/>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				content: '',
+				items: []
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
